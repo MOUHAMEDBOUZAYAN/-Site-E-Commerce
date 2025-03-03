cb4f1615abf61571850803d92e7cabb85300fe58
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-03-03 14:13:13 +0000

    first commit, env set

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-5xl font-bold text-red-500 underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
